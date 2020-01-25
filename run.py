a4049bd76530a5de0b3691a7b64fb4ce40f177e0
mfatima <mfatima@an10.io>
2020-01-25 16:04:35 +0500

    get user details

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from controllers import resources
 from controllers import Service as rt
 from services import message, email
+from controllers import users
 from controllers import generalQuestions
 from models.RevokedTokens import RevokedTokens
 from flask_mongoengine import MongoEngine
@@ -33,19 +34,29 @@ api.add_resource(resources.AddEmergencyContact, '/api/v1/registration/user/<user
 api.add_resource(resources.AddEmployementHistory, '/api/v1/registration/user/<userId>/employement-details', methods=['POST'])
 api.add_resource(resources.AddGeneralQuestionAnswer, '/api/v1/registration/user/<userId>/general-question-answers', methods=['POST'])
 api.add_resource(resources.UpdateUserType, '/api/v1/registration/user/<userId>/update-user-type', methods=['PATCH'])
-api.add_resource(resources.test, '/api/v1/test')
-api.add_resource(resources.UserLogin, '/api/v1/login')
-api.add_resource(resources.UserLogoutAccess, '/api/v1/logout/access')
-api.add_resource(resources.UserLogoutRefresh, '/api/v1/logout/refresh')
-api.add_resource(resources.TokenRefresh, '/api/v1/token/refresh')
-# api.add_resource(rt.RecruitmentTest, '/api/v1/recruitmentTest/<id>', methods=['GET', 'PATCH', 'DELETE'], )
-api.add_resource(rt.Service, '/api/v1/service', methods=['GET', 'POST'])
-api.add_resource(message.sendMessage, '/api/v1/sendMessage/<number>', methods=['POST'])
-api.add_resource(email.sendEmail, '/api/v1/sendEmail/<email>', methods=['POST'])
 api.add_resource(resources.ReferenceRegistration, '/api/v1/registration/user/<userId>/references', methods=['POST'])
 api.add_resource(resources.AddServices, '/api/v1/registration/user/<userId>/services', methods=['POST'])
+# Healt check
+api.add_resource(resources.test, '/api/v1/test')
+# Login
+api.add_resource(resources.UserLogin, '/api/v1/login')
+# Logout
+api.add_resource(resources.UserLogoutAccess, '/api/v1/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/api/v1/logout/refresh')
+# Token refresh
+api.add_resource(resources.TokenRefresh, '/api/v1/token/refresh')
+# Services
+api.add_resource(rt.Service, '/api/v1/service', methods=['GET', 'POST'])
+# External email and message service
+api.add_resource(message.sendMessage, '/api/v1/sendMessage/<number>', methods=['POST'])
+api.add_resource(email.sendEmail, '/api/v1/sendEmail/<email>', methods=['POST'])
+# Add general questions
 api.add_resource(generalQuestions.GeneralQuestions, '/api/v1/general-questions', methods=['POST', 'GET'])
 # api.add_resource(generalQuestions.GeneralQuestions, '/api/general-question/<id>/update', methods=['PUT'])
+# User
+api.add_resource(users.users, '/api/v1/users', methods=['GET'])
+api.add_resource(users.user, '/api/v1/users/<userId>', methods=['GET'])
+
 
 
 #Registering database
