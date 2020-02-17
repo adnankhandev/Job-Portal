7b5cefdec51d494fa658075fca3c011f3b4ce74a
Adnan A. Khan <adnanak556@gmail.com>
2020-02-17 22:15:42 +0500

    minor changes

--- models/Jobs.py
+++ models/Jobs.py
@@ -4,7 +4,8 @@ import json
 class Jobs(me.Document):
     meta = {'collection': 'jobs'}
 
-    title = me.StringField()
+    job_title = me.StringField()
+    customer_name = me.StringField()
     job_description = me.StringField()
     required_services = me.ListField(me.ReferenceField('Services'), reverse_delete_rule=1)
     start_date_to_apply = me.DateTimeField()
